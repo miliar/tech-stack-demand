ba2061688d373be27321a2e67dc6f6ee35869164
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-08-29 23:59:48 +0200

    Remove false positive keywords

--- job_keywords_parser/parser.py
+++ job_keywords_parser/parser.py
@@ -7,8 +7,9 @@ class JobKeywordsParser:
         self.text = text
 
     def get(self):
-        return ' '.join(requests.get('http://keywords_api:4000/keywords/' +
-                                     self._preprocess_text()).json())
+        keywords = ' '.join(requests.get('http://keywords_api:4000/keywords/' +
+                                         self._preprocess_text()).json())
+        return self._exclude_false_positive(keywords)
 
     def _preprocess_text(self):
         text = ''.join(
@@ -16,6 +17,28 @@ class JobKeywordsParser:
         text = text.replace('. ', ' ').replace(
             '.\n', ' ').replace('#', 'sharp')
         return ' '.join(text.lower().split())
+
+    def _exclude_false_positive(self, keywords):
+        false_positive = ['for-loop', 'testing', 'web', 'service', 'process',
+                          'this', 'build', 'performance', 'frameworks', 'join',
+                          'if-statement', 'makefile', 'architecture', 'task', 'time',
+                          'get', 'stack', 'shared-libraries', 'focus', 'field',
+                          'dynamic', 'position', 'optimization', 'key', 'while-loop',
+                          'find', 'include', 'set', 'client', 'interface',
+                          'computer-vision', 'background', 'components', 'location', 'date',
+                          'directory', 'methods', 'coffeescript', 'controls', 'types',
+                          'range', 'hyperlink', 'package', 'class', 'structure',
+                          'version', 'report', 'session', 'email', 'search',
+                          'numbers', 'path', 'image', 'forms', 'colors',
+                          'object', 'view', 'scope', 'reference', 'try-catch',
+                          'installation', 'configuration', 'function', 'coordinates', 'input',
+                          'tags', 'math', 'command', 'connection', 'alignment',
+                          'modul', 'list', 'iteration', 'logic', 'express',
+                          'load', 'prepared-statement', 'conditional-statements', 'dependencies',
+                          'max', 'layout', 'loops', 'text', 'size',
+                          'click', 'certificate', 'request', 'map', 'button', 'migration'
+                          ]
+        return ' '.join([k for k in keywords.split() if k not in false_positive])
 
 
 if __name__ == '__main__':
