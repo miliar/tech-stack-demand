a573c96839c293a5ba7cec7f89f94a24c11e0d46
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-08-31 17:06:42 +0200

    Change config

--- job_description_scraper/config.py
+++ job_description_scraper/config.py
@@ -1,5 +1,5 @@
 KAFKA = 'kafka:9092'
-KAFKA_CONSUMER_GROUP = 'description_scrapers'
+KAFKA_CONSUMER_GROUP = 'scrapers'
 KAFKA_INPUT_TOPIC = 'job_urls'
 KAFKA_OUTPUT_TOPIC = 'job_descriptions'
 KAFKA_ERROR_TOPIC = 'errors'
