600668a92aed63d97a2a6901597c116f69de2fe2
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-09-01 15:14:45 +0200

    Add testing for JobDescriptionParser

---
+++ job_keywords_parser/test/test_job_keywords_parser.py
@@ -0,0 +1,16 @@
+import unittest
+from job_keywords_parser import JobKeywordsParser
+
+
+class TestJobKeywordsParser(unittest.TestCase):
+    def setUp(self):
+        self.expected_keywords = set('ios java swift'.split())
+        self.job_description = ('You are an open-minded and creative team player enjoying '
+                                'constructive code and architecture discussions '
+                                'Writing clean, maintainable and well documented code comes naturally'
+                                'Android (Java) and/or iOS (Swift) experience is a plus '
+                                'Fluency in English, German language skills are a plus though not mandatory')
+
+    def test_get(self):
+        keywords = set(JobKeywordsParser(self.job_description).get().split())
+        self.assertEqual(keywords, self.expected_keywords)
