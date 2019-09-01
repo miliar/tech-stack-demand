56218d84970fd5c765c38d37c8474c8ef4d0bbbd
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-09-01 22:10:49 +0200

    Fix test

--- job_keywords_parser/test/test_job_keywords_parser.py
+++ job_keywords_parser/test/test_job_keywords_parser.py
@@ -4,10 +4,10 @@ from job_keywords_parser import JobKeywordsParser
 
 class TestJobKeywordsParser(unittest.TestCase):
     def setUp(self):
-        self.expected_keywords = set('ios java swift'.split())
+        self.expected_keywords = set('android ios java swift'.split())
         self.job_description = ('You are an open-minded and creative team player enjoying '
                                 'constructive code and architecture discussions '
-                                'Writing clean, maintainable and well documented code comes naturally'
+                                'Writing clean, maintainable and well documented code comes naturally '
                                 'Android (Java) and/or iOS (Swift) experience is a plus '
                                 'Fluency in English, German language skills are a plus though not mandatory')
 
