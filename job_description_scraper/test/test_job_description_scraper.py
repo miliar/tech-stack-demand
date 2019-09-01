c1f2f1ca6e209e717bf9b9396c2365ad463dd60d
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-09-01 14:37:37 +0200

    Add testing for JobDescriptionScraper

---
+++ job_description_scraper/test/test_job_description_scraper.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch
+from bs4 import BeautifulSoup
+from job_description_scraper import JobDescriptionScraper
+
+
+class TestJobDescriptionScraper(unittest.TestCase):
+    def setUp(self):
+        with open("test/fixture/indeed.html", "rb") as description_page:
+            self.description_page = BeautifulSoup(
+                description_page, 'html.parser')
+        self.expected_job_description = 'Test description'
+        self.expected_job_company = 'Test company'
+
+    def test_get_job_description(self):
+        with patch('job_description_scraper.get_html') as mocked_get:
+            mocked_get.side_effect = lambda url: self.description_page
+            job_description = JobDescriptionScraper(
+                'https://test').get_job_description()
+            self.assertEqual(job_description, self.expected_job_description)
+
+    def test_get_job_company(self):
+        with patch('job_description_scraper.get_html') as mocked_get:
+            mocked_get.side_effect = lambda url: self.description_page
+            job_company = JobDescriptionScraper(
+                'https://test').get_job_company()
+            self.assertEqual(job_company, self.expected_job_company)
