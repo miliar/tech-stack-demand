1fc5c4b490a1ba28bc65e79b501d635d0951ebee
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-09-06 16:05:54 +0200

    Add visualizer

---
+++ visualizer/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
